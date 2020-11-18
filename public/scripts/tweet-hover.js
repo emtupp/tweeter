e43c84cac54cfdf3adf17c06e06e866e333fed7c
emtupp <emilyc.tupper@gmail.com>
2020-11-18 20:49:16 +0000

    fixed nav bar and finished hover on tweet box, missing avatar

--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   console.log('ok');
   $("#display-tweets").mouseover(function() {
     $(".display-tweets").css("box-shadow", "5px 5px #4057a162");
-    $(".display-tweets").css("font-weight", "medium");
+    $(".display-tweets").css("font-weight", "bold");
     $(".uid").css("color", "#4057a162");
     $("#display-tweets").mouseleave(function() {
       $(".display-tweets").css("box-shadow", "none");
       $(".display-tweets").css("font-weight", "normal");
       $(".uid").css("color", "transparent");
-    })
+    });
   });
 });
