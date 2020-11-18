53adc5c2aaed6d8a491ae88af0578504305d6155
emtupp <emilyc.tupper@gmail.com>
2020-11-18 20:17:11 +0000

    adding the hover on tweet box

---
+++ public/scripts/tweet-hover.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  console.log('ok');
+  $("#display-tweets").mouseover(function() {
+    $(".display-tweets").css("box-shadow", "5px 5px #4057a162");
+    $(".display-tweets").css("font-weight", "medium");
+    $(".uid").css("color", "#4057a162");
+    $("#display-tweets").mouseleave(function() {
+      $(".display-tweets").css("box-shadow", "none");
+      $(".display-tweets").css("font-weight", "normal");
+      $(".uid").css("color", "transparent");
+    })
+  });
+});
