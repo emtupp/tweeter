d143e9035648f9eb314e806ee473b32c6bdda78b
emtupp <emilyc.tupper@gmail.com>
2020-11-18 23:56:18 +0000

    fixing error in css for tweet hover

--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   console.log('ok');
-  $("#display-tweets").mouseover(function() {
-    $(".display-tweets").css("box-shadow", "5px 5px #4057a162");
-    $(".display-tweets").css("font-weight", "bold");
+  $("#tweet").mouseover(function() {
+    $(".tweet").css("box-shadow", "5px 5px #4057a162");
+    $(".tweet").css("font-weight", "bold");
     $(".uid").css("color", "#4057a162");
-    $("#display-tweets").mouseleave(function() {
-      $(".display-tweets").css("box-shadow", "none");
-      $(".display-tweets").css("font-weight", "normal");
+    $("#tweet").mouseleave(function() {
+      $(".tweet").css("box-shadow", "none");
+      $(".tweet").css("font-weight", "normal");
       $(".uid").css("color", "transparent");
     });
   });
