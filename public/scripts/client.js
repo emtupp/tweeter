0bcd79c2736517d45d65701af4ed5930acbe8865
emtupp <emilyc.tupper@gmail.com>
2020-11-19 05:19:44 +0000

    progress on get request with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,43 +1,15 @@
-const db = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const preventAction = (event, action) => {
+//   event.preventDefault()
 
-
-
-
-const preventAction = (event, action) => {
-  event.preventDefault()
-
-  $
-    .ajax({
-      url: "/tweets",
-      method: "POST",
-      data: $('form').serialize()
-    })
-    .then(res => action(res))
-    .catch(err => console.log(err))
-}
+//   $
+//     .ajax({
+//       url: "/tweets",
+//       method: "POST",
+//       data: $('form').serialize()
+//     })
+//     .then(res => action(res))
+//     .catch(err => console.log(err))
+// }
 
 const createTweetElement = function(tweet) {
 let $tweet = $(`<article class="tweet" id="tweet">
@@ -67,14 +39,19 @@ const renderTweets = function(tweets) {
 
 $(document).ready(function() {
   $('form').on("submit", function(event) {
+    console.log('form submitted, preventing page change')
     event.preventDefault();
-    $
-    .ajax({
+    $.ajax({
       url: "/tweets",
       method: "POST",
       data: $('form').serialize()
     })
-    console.log($('form').serialize())
+    .then(() => $.ajax('/tweets', { method: 'GET' }))
+    .then(function (data) {
+      return data;
+    })
+    .then(renderTweets(data))
+    .catch(err => console.log(err))
   });
-  renderTweets(db);
+  // renderTweets(db);
 });
