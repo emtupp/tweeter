2ab70ea74d8ebf894e23c93d6ed0a74773b2de79
emtupp <emilyc.tupper@gmail.com>
2020-11-20 03:46:55 +0000

    fixed char count to use parent and children instead of .counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
   $("#tweet-text").on("input", function() {
     let counter = 140 - $(this).val().length;
-    $(".counter").text(counter);
-    // don't use .counter, but children and parent
-    // condition () ternary operator
+    $(this).parent().children('.tweet-wordcount').children('output').text(counter);
     if (counter >= 0) {
       $(".counter").css("color", "#545149");
     } else {
